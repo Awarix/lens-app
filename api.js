e2e7576764941abbd0a569124128226c6b8e132f
Awarix <mishmishmuga@gmail.com>
2022-10-15 20:42:40 +0300

    first commit

---
+++ api.js
@@ -0,0 +1,228 @@
+import { createClient } from 'urql'
+
+const APIURL = "https://api.lens.dev"
+
+export const client = new createClient({
+  url: APIURL
+})
+
+export const exploreProfiles = `
+  query ExploreProfiles {
+    exploreProfiles(request: { sortCriteria: MOST_FOLLOWERS }) {
+      items {
+        id
+        name
+        bio
+        isDefault
+        attributes {
+          displayType
+          traitType
+          key
+          value
+        }
+        followNftAddress
+        metadata
+        handle
+        picture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            chainId
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+        }
+        coverPicture {
+          ... on NftImage {
+            contractAddress
+            tokenId
+            uri
+            chainId
+            verified
+          }
+          ... on MediaSet {
+            original {
+              url
+              mimeType
+            }
+          }
+        }
+        ownedBy
+        dispatcher {
+          address
+          canUseRelay
+        }
+        stats {
+          totalFollowers
+          totalFollowing
+          totalPosts
+          totalComments
+          totalMirrors
+          totalPublications
+          totalCollects
+        }
+        followModule {
+          ... on FeeFollowModuleSettings {
+            type
+            contractAddress
+            amount {
+              asset {
+                name
+                symbol
+                decimals
+                address
+              }
+              value
+            }
+            recipient
+          }
+          ... on ProfileFollowModuleSettings {
+          type
+          }
+          ... on RevertFollowModuleSettings {
+          type
+          }
+        }
+      }
+      pageInfo {
+        prev
+        next
+        totalCount
+      }
+    }
+  }
+  `
+
+  export const getPublications = `
+  query Publications($id: ProfileId!, $limit: LimitScalar) {
+    publications(request: {
+      profileId: $id,
+      publicationTypes: [POST],
+      limit: $limit
+    }) {
+      items {
+        __typename 
+        ... on Post {
+          ...PostFields
+        }
+      }
+    }
+  }
+  fragment PostFields on Post {
+    id
+    metadata {
+      ...MetadataOutputFields
+    }
+    createdAt
+  }
+  fragment MetadataOutputFields on MetadataOutput {
+    name
+    description
+    content
+    media {
+      original {
+        ...MediaFields
+      }
+    }
+    attributes {
+      displayType
+      traitType
+      value
+    }
+  }
+  fragment MediaFields on Media {
+    url
+    mimeType
+  }
+`
+
+export const getProfile = `
+query Profile($id: ProfileId!) {
+  profile(request: { profileId: $id }) {
+    id
+    name
+    bio
+    attributes {
+      displayType
+      traitType
+      key
+      value
+    }
+    followNftAddress
+    metadata
+    isDefault
+    picture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        verified
+      }
+      ... on MediaSet {
+        original {
+          url
+          mimeType
+        }
+      }
+      __typename
+    }
+    handle
+    coverPicture {
+      ... on NftImage {
+        contractAddress
+        tokenId
+        uri
+        verified
+      }
+      ... on MediaSet {
+        original {
+          url
+          mimeType
+        }
+      }
+      __typename
+    }
+    ownedBy
+    dispatcher {
+      address
+      canUseRelay
+    }
+    stats {
+      totalFollowers
+      totalFollowing
+      totalPosts
+      totalComments
+      totalMirrors
+      totalPublications
+      totalCollects
+    }
+    followModule {
+      ... on FeeFollowModuleSettings {
+        type
+        amount {
+          asset {
+            symbol
+            name
+            decimals
+            address
+          }
+          value
+        }
+        recipient
+      }
+      ... on ProfileFollowModuleSettings {
+        type
+      }
+      ... on RevertFollowModuleSettings {
+        type
+      }
+    }
+  }
+}
+`
--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      'ipfs.infura.io',
+      'statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com',
+      'lens.infura-ipfs.io',
+      'source.unsplash.com',
+      ""
+    ],
+  },
 }
 
 module.exports = nextConfig
